7b3ac2ac343239567bc5eebe21192d7bd2ae0849
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-09-19 16:29:08 +0300

    add modal

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello test");
+const openModalBtn = document.querySelector(".menu-open");
+const closeModalBtn = document.querySelector(".menu-close");
+const backdrop = document.querySelector(".backdrop");
+const body = document.querySelector("body");
+
+function onOpenModal() {
+  body.style.overflow = "hidden";
+  backdrop.classList.add("is-open");
+}
+
+function onCloseModal() {
+  backdrop.classList.remove("is-open");
+  body.style.overflow = "auto";
+}
+
+openModalBtn.addEventListener("click", onOpenModal);
+closeModalBtn.addEventListener("click", onCloseModal);
